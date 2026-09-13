0519c25775ad5e45dccc3f2d98de4a7104385707
TawodzeraT <tavongatawodzera6@gmail.com>
2026-09-12 20:14:48 -0400

    Update Firebase configuration with actual values

--- firebase/config.js
+++ firebase/config.js
@@ -1,12 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY_HERE",
-  authDomain: "PASTE_YOUR_AUTH_DOMAIN_HERE",
+  apiKey: "AIzaSyBKUtw56MHta0i8iErO6Qm2f8bU1PBuLxk",
+  authDomain: "selly-bake-house-web.firebaseapp.com",
   projectId: "selly-bake-house-web",
-  storageBucket: "PASTE_YOUR_STORAGE_BUCKET_HERE",
-  messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID_HERE",
-  appId: "PASTE_YOUR_APP_ID_HERE"
+  storageBucket: "selly-bake-house-web.firebasestorage.app",
+  messagingSenderId: "458663403688",
+  appId: "1:458663403688:web:0d4a9a3bc0c92a11fc629d",
+  measurementId: "G-09Q1L3PYNW"
 };
 
 export default firebaseConfig;
-
------BEGIN PRIVATE KEY-----
